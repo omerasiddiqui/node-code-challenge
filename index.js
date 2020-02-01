5a77b6374d917831bb23c62d64f2030a3937bcbc
Omer Siddiqui <omersiddiqui@Omers-MacBook-2.lan>
2020-02-01 14:26:28 -0700

    bug fix - added punctuation to end of sentence

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ function createEpisodeObj() {
         let epiName = item.name,
             split = epiName.split(': '),
             shortTitle = split[1];
-        
-    // date
+
+        // date
         function Epoch(date) {
             return Math.round(new Date(date).getTime() / 1000.0);
         }
 
-    // summary
+        // summary
         let summary = item.summary;
 
         if (summary != null) {
@@ -50,19 +50,19 @@ function createEpisodeObj() {
                 firstSentence = multiSplit[1];
 
                 if (!firstSentence.endsWith('.')) {
-                    firstSentence.concat('', '.');
+                    firstSentence = firstSentence.concat('', '.');
                 }
             }
         } else {
             firstSentence = 'n/a'
         }
 
-    // sequence
+        // sequence
         let sequence = `S${item.season}E${item.number}`,
             id = item.id,
             airTimeStamp = Epoch(item.airstamp);
 
-    // episode object
+        // episode object
         const episodeObj = {
             [id]: {
                 "sequenceNumber": sequence,
@@ -71,7 +71,7 @@ function createEpisodeObj() {
                 "shortSummary": firstSentence
             }
         }
-    // push to episodes array
+        // push to episodes array
         episodes.push(episodeObj);
     })
 
